82848c83890340a8610844a9bfaba75097829d46
roman-ahmadzeia <romanahmadzeia@ontariotechu.net>
2023-12-07 13:49:09 -0500

    ios working - roman

---
+++ carsellingapp/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'statecontrollers/authtree.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+void main() async{
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();
+runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 247, 247)),
+        useMaterial3: true,
+      ),
+      home: const authTree(),
+    );
+  }
+}
+
